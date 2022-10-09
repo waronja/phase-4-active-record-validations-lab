9a07b484532b0c678ee23249927ac53f9ae15a97
simon waronja <simon.waronja@student.moringaschool.com>
2022-10-09 20:27:02 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, {:length => { :minimum => 250 }}
+    validates :summary, {:length => { :maximum => 250 }}
+    validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
+    validate :title_should_contain
+    # validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+    TITLE_NOT_CONTAIN = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top/i,
+        /Guess/i
+
+    ]
+
+    def title_should_contain
+        if TITLE_NOT_CONTAIN.none? { |word| word.match title}
+            errors.add(:title, "Title not clickbaity enough")
+        end
+    end
 end
